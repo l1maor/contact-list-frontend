f59cc08d2a01fe38422825b8066759f3532462b8
l1maor <lim4or@gmail.com>
2025-03-01 07:11:15 -0500

    added prime react custom theme

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+export const customTheme = {
+  colors: {
+    background: '#fff2d7',
+    cardBackground: '#e2d5ff',
+    avatarBackground: '#ff7f50',
+    buttonBackground: '#ffc107',
+    searchBackground: '#d3d3d3',
+    text: {
+      primary: '#000000',
+      secondary: '#666666'
+    }
+  }
+};
