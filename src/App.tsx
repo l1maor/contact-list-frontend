bcd368faffd04a7927c977f264ec1c2f8c644e7f
l1maor <lim4or@gmail.com>
2025-03-03 15:16:39 -0500

    added toast component and behavior to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toast } from 'primereact/toast';
+import { useRef, useEffect } from 'react';
 import ContactListPage from './pages/ContactListPage/ContactListPage';
 import ContactViewPage from './pages/ContactViewPage/ContactViewPage';
 import ContactFormPage from './pages/ContactFormPage/ContactFormPage';
+import { setToastRef } from './utils/toast';
 
 function App() {
+  const toast = useRef<Toast>(null);
+
+  // Set toast ref on mount
+  useEffect(() => {
+    if (toast.current) {
+      setToastRef(toast.current);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
+      <Toast ref={toast} position="top-right" />
       <Routes>
         <Route path="/" element={<ContactListPage />} />
         <Route path="/contact/:contactId" element={<ContactViewPage />} />
